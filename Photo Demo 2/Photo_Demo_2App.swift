5284e9e26add8f1f6ff60e58f872f652493e37f5
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-04-01 19:43:41 -0400

    Initial Commit

---
+++ Photo Demo 2/Photo_Demo_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_Demo_2App.swift
+//  Photo Demo 2
+//
+//  Created by Lori Rothermel on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Photo_Demo_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
